0355b756aef6d89cc9d35eb83ea87be02d5e678a
Slashyo <tom.bik@hotmail.be>
2023-03-06 15:12:18 +0100

    J'arrête pour ajd

--- view/homePage.php
+++ view/homePage.php
@@ -19,14 +19,14 @@ include 'include/menuDarkMode.php';
     <h1>WEB DEVELOPER</h1>
 </div>
 
-    <div id="childBlock">
+<div id="childBlock">
     <h2>HEHEHEHE</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque labore minima debitis enim vitae, voluptas dicta ab aperiam rem! Et.</p>
-    </div>
-    <div id="childBlock1">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, hic. Dolorem debitis, reiciendis ad laborum quasi natus recusandae et distinctio maiores quam alias, vitae tempora molestias impedit adipisci voluptates unde deserunt earum fugit nam eius. Alias deserunt quam ducimus obcaecati facilis repudiandae odio atque praesentium! Quia natus harum quis atque dolorem eveniet est porro accusamus non, corrupti fugiat molestias saepe eius ipsum sequi esse pariatur! Suscipit minima consequatur nesciunt eligendi perspiciatis repellendus architecto libero expedita, obcaecati tempora excepturi ut nam hic, corporis reprehenderit, dignissimos earum? Vel deserunt, dignissimos doloribus, consequuntur accusamus velit distinctio, ex consequatur alias placeat magnam amet. Quod.</p>
+</div>
+<div id="childBlock1">
     <h2>HEHEHEHE</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, mollitia ratione fugiat esse similique voluptatum! Dolorem error unde vero perferendis!</p>
-    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod eius veritatis odio cupiditate repellendus soluta, ea exercitationem doloribus nemo! Distinctio, possimus totam recusandae facilis nostrum sequi suscipit eos alias illo delectus iusto quas porro dolor reprehenderit aut velit maiores maxime perspiciatis molestias numquam necessitatibus. Eaque praesentium ea corrupti, eveniet fugit exercitationem dicta facilis sunt aliquid officia ullam excepturi. Vitae necessitatibus quisquam obcaecati modi illo, nesciunt, aliquam minima vero dolorum exercitationem iure! Repellat culpa cum consectetur, at quidem quasi, enim similique optio dignissimos explicabo deserunt reiciendis quibusdam dolorem consequuntur nemo, maiores dicta velit? Cumque optio cum quas, aliquid accusantium libero reprehenderit!</p>
+</div>
     <?php
 include 'include/footer.php'
     ?>
