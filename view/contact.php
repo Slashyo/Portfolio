0ba5c4588bb1849095bf5bf90e2e8784e918c79b
Slashyo <tom.bik@hotmail.be>
2023-03-06 11:51:28 +0100

    Contact

--- view/contact.php
+++ view/contact.php
@@ -6,15 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menuDarkMode.css">
     <script src="js/script.js" defer></script>
-    <title>Contact</title>
+    <title>Formulaire de contact</title>
 </head>
 <body>
+<div id="gridmenu">
     <?php
 include 'include/menuDarkMode.php';
     ?>
+    <h3>Contactez moi</h3>
+<form action="#" method="POST">
+		<label for="nom">Nom</label>
+		<input type="text" id="nom" name="nom" placeholder="Votre nom" required>
 
+		<label for="email">Email</label>
+		<input type="text" id="email" name="email" placeholder="Votre adresse email" required>
+
+		<label for="message">Message</label>
+		<textarea id="message" name="message" placeholder="Votre message" required></textarea>
+
+		<input type="submit" value="Envoyer">
+</form>
     <?php
 include 'include/footer.php'
     ?>
+</div>
 </body>
 </html>
