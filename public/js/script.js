3e1f944c5762eed23aff762e5e4d42bfa108c3fc
Slashyo <tom.bik@hotmail.be>
2023-03-08 08:11:30 +0100

    LocalStorage DarkMode

--- public/js/script.js
+++ public/js/script.js
@@ -8,9 +8,28 @@ const menuClose = document.querySelector('.menu-close');
 
 /* Fonction pour le DARK MODE */
 
+// Initialisation de l'état du mode sombre
+let isDarkMode = false;
+
+// Vérification si l'état du mode sombre existe déjà dans le localstorage
+if (localStorage.getItem('isDarkMode') !== null) {
+  isDarkMode = JSON.parse(localStorage.getItem('isDarkMode'));
+  body.classList.toggle('dark-mode', isDarkMode);
+  if (isDarkMode) {
+    modeSwitch.classList.remove('dark-mode');
+    modeSwitch.classList.add('light-mode');
+  } else {
+    modeSwitch.classList.remove('light-mode');
+    modeSwitch.classList.add('dark-mode');
+  }
+}
+
+// Fonction de commutation du mode sombre
 modeSwitch.addEventListener('click', function() {
   body.classList.toggle('dark-mode');
-  if (body.classList.contains('dark-mode')) {
+  isDarkMode = body.classList.contains('dark-mode');
+  localStorage.setItem('isDarkMode', JSON.stringify(isDarkMode));
+  if (isDarkMode) {
     modeSwitch.classList.remove('dark-mode');
     modeSwitch.classList.add('light-mode');
   } else {
