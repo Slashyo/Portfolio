7c09c8a3f7f0ec6242a1dfbc8abe85801779d6a1
Slashyo <tom.bik@hotmail.be>
2023-03-15 15:16:28 +0100

    15-03

--- model/envoiMail.php
+++ model/envoiMail.php
@@ -41,51 +41,62 @@ if(isset($_POST['nom'], $_POST['email'], $_POST['message'] )){
 
     # si les champs sont bons (ici vide, donc une seule erreur générale)
     if(!empty($nom)&&!empty($mail)&&!empty($message)){
-        
+
         # insertion partie SQL
         $sqlInsert = "INSERT INTO `contacts` (`nom`, `email`, `message`) VALUES ('$nom','$mail','$message');";
-
+    
         # requête avec try catch
         try{
             # requête
             mysqli_query($dbconnect,$sqlInsert);
-            
-// DEMANDER A MIKKKKKKAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
+    
             # envoi du mail
             $destinataire = 'tom.bik@hotmail.be'; // remplacer par l'adresse mail souhaitée
             $sujet = 'Nouveau message du formulaire de contact';
-            $corps_message = "Nom: $nom\nEmail: $mail\nMessage: $message";
-            mail($destinataire, $sujet, $corps_message);
-            
-            # si pas d'erreur création du texte
-            $afficheVal ="Merci pour votre message !";
-
+            $message_html = "
+                <html>
+                    <body>
+                        <h2>Nouveau message du formulaire de contact :</h2>
+                        <p><strong>Nom :</strong> $nom</p>
+                        <p><strong>Email :</strong> $mail</p>
+                        <p><strong>Message :</strong> $message</p>
+                    </body>
+                </html>";
+            $headers = array(
+                'MIME-Version: 1.0',
+                'Content-type: text/html; charset=utf-8',
+                'From: Mon Site Web <noreply@monsite.com>',
+                'Reply-To: '.$nom.' <'.$mail.'>',
+                'X-Mailer: PHP/'.phpversion()
+            );
+            $success = mail($destinataire, $sujet, $message_html, implode("\r\n", $headers));
+    
+            if($success){
+                # si pas d'erreur création du texte
+                $afficheVal ="Merci pour votre message !";
+            }else{
+                # sinon erreur
+                $afficheErr = "Il y a eu un problème lors de l'envoi de votre message, veuillez réessayer";
+            }
+    
         }catch(Exception $e){
-           # echo $e->getCode();
-
-           # avec le code erreur SQL on peut faire des erreurs différentes, idem avec le $e->getMessage() etc...
             if($e->getCode()==1406){
                 # création de l'erreur
                 $afficheErr = "Le champ 'nom' est trop long";
-
             }elseif($e->getCode()==1062){
                 # création de l'erreur
                 $afficheErr = "Vous êtes déjà inscrit avec ce mail";
             }else{
-                $afficheErr = "Erreur incconu";
+                $afficheErr = "Erreur inconnue";
             }
-            
         }
-
-
-    # sinon erreur
+    
     }else{
         # erreur lors de l'inscription
         $afficheErr = "Il y a eu un problème lors de l'envoi de votre message, veuillez réessayer";
     }
-}
-}
+    }
+}    
 
 # fermeture de connexion  (bonnes pratiques)
 mysqli_close($dbconnect);
--- public/index.php
+++ public/index.php
@@ -6,5 +6,3 @@ require_once "../model/lienModel.php";
 require_once "../model/envoiMail.php";
 require_once "../model/travauxModel.php";
 require_once "../controller/publicController.php";
-
-
