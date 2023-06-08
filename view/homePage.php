543a3b90bac6d6eb9886a5bb8cf76e5c19576aef
Slashyo <tom.bik@hotmail.be>
2023-06-08 15:18:55 +0200

    Changement ordre du code

--- view/homePage.php
+++ view/homePage.php
@@ -25,6 +25,9 @@ include 'include/menuDarkMode.php';
     <h1>WEB DEVELOPER</h1>
 </div>
 <hr id="first">
+<div id="childBlock2">
+<p>Je suis un jeune développeur web en formation, passionné par les technologies et les possibilités créatives que le développement web offre.</br></br>Actuellement, je suis en train de perfectionner mes compétences en matière de conception et de développement de sites web, en utilisant des technologies telles que HTML, CSS, JavaScript, PHP, SQL.</br></br><!--Mon désir d'apprendre et d'explorer de nouvelles idées m'anime constamment, tout en travaillant dur pour créer des sites web fonctionnels, intuitifs et esthétiquement plaisants.</br></br> Je suis confiant dans mes compétences et impatient de travailler avec des clients potentiels pour créer des solutions de développement web innovantes et personnalisées.</br></br>!--> Mon portfolio est une vitrine de mes compétences et de mon expérience en matière de développement web, démontrant ma capacité à créer des sites web fonctionnels et esthétiquement agréables pour une variété de projets et de clients.</p>
+</div>
 <div id="childBlock">
 <h3>LAST PROJECTS</h3>
 <a href="?page=travaux" target="_blank" ><img src="img/Capture_d’écran_2023-03-13_112644.png" width="150px" height="auto" alt=""></a>
@@ -35,9 +38,6 @@ include 'include/menuDarkMode.php';
 <a href="?page=contact" target="_blank"><img src="img/Capture_d’écran_2023-03-13_112644.png" width="150px" height="auto" alt=""></a>
 </div>
 <hr id="deux">
-<div id="childBlock2">
-<p>Je suis un jeune développeur web en formation, passionné par les technologies et les possibilités créatives que le développement web offre.</br></br>Actuellement, je suis en train de perfectionner mes compétences en matière de conception et de développement de sites web, en utilisant des technologies telles que HTML, CSS, JavaScript, PHP, SQL.</br></br><!--Mon désir d'apprendre et d'explorer de nouvelles idées m'anime constamment, tout en travaillant dur pour créer des sites web fonctionnels, intuitifs et esthétiquement plaisants.</br></br> Je suis confiant dans mes compétences et impatient de travailler avec des clients potentiels pour créer des solutions de développement web innovantes et personnalisées.</br></br>!--> Mon portfolio est une vitrine de mes compétences et de mon expérience en matière de développement web, démontrant ma capacité à créer des sites web fonctionnels et esthétiquement agréables pour une variété de projets et de clients.</p>
-</div>
 
     <?php
 include 'include/footer.php'
